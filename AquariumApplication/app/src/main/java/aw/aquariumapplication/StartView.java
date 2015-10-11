971c6322151527096e5ba06348eee454538dc343
tsutsui <tsutsui@alphawave.co.jp>
2015-10-12 01:57:59 +0900

    スタート画面を追加　コミット漏れ

---
+++ AquariumApplication/app/src/main/java/aw/aquariumapplication/StartView.java
@@ -0,0 +1,16 @@
+package aw.aquariumapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class StartView extends View {
+
+    public StartView(Context context) {
+        super(context);
+    }
+
+    public StartView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+}
